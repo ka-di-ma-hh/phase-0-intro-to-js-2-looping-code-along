baa0f2a5dcd19942a7c6c6531770cb4688741666
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 15:05:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards() {
+    let names = ["Guadalupe", "Ollie", "Aki"];
+      let messages = [];
+      for (let i = 0; i < names.length; i++) {
+          messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      }
+       return messages
+  }
+  console.log(writeCards())
+  
+  function countDown(number) {
+      for (let i = number; i >= 0; i--) {
+          console.log(i);
+      }
+  }
+  console.log(countDown)
